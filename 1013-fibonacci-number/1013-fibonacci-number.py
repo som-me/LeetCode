dc6163a2a259d8c246f3ef666f0662994590ebae
Soumya Ranjan Meher <157711914+som-me@users.noreply.github.com>
2025-09-06 02:17:04 +0530

    Time: 28 ms (97.1%), Space: 17.7 MB (63.8%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        #base case
-        if n == 0 or n == 1: return n
-        #recursive case
-        return self.fib(n-1) + self.fib(n-2)
+        dp = [-1]*(n+1)
+        def helper(k):
+            #base case
+            if k == 0 or k == 1: return k
+
+            #check if already present
+            if dp[k] != -1: return dp[k]
+
+            #recursive case
+            dp[k] = helper(k-1) + helper(k-2)
+            return dp[k]
+        return helper(n)
