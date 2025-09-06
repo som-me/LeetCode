893fb71c24e7494c2363767f94f80fc1f6871217
Soumya Ranjan Meher <157711914+som-me@users.noreply.github.com>
2025-09-06 14:32:24 +0530

    Time: 41 ms (54.42%), Space: 17.9 MB (23.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,14 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
+        # We created a list for cache
         dp = [-1]*(n+1)
-        def helper(k):
-            #base case
-            if k == 0 or k == 1: return k
-
-            #check if already present
-            if dp[k] != -1: return dp[k]
-
-            #recursive case
-            dp[k] = helper(k-1) + helper(k-2)
-            return dp[k]
+        # Lets use this list for storing the memoization values..
+        def helper(i):
+            #Base case:
+            if i == 0:
+                return 0
+            if i == 1:
+                return 1
+            #Check if the value of i exists in the DP cache..
+            if dp[i] != -1:
+                return dp[i]
+            #If dp[i] == -1 then, initailze it with the new value recursively and return dp[i]
+            dp[i] = helper(i-1) + helper(i-2)
+            return dp[i]
         return helper(n)
