46d09bea163668a40ef6ab8f90b51dd486923852
Soumya Ranjan Meher <157711914+som-me@users.noreply.github.com>
2025-09-06 14:11:13 +0530

    Time: 0 ms (100%), Space: 17.6 MB (95.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # Simply used a Bottom-Up approach to solve this DP..
         one, two = 1, 1
         for i in range(n-1):
             temp = one
