8b77b8be2b241f28cf766bd9317b6b3d1fd8b061
Soumya Ranjan Meher <157711914+som-me@users.noreply.github.com>
2026-03-26 14:32:09 +0530

    Time: 27 ms (94.44%), Space: 28.7 MB (87.34%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        k = 10
+        left = 0
+        seen = set()
+        result = set()
+        for r in range(len(s) - k + 1):
+            window = s[r:r+k]
+            if window in seen:
+                result.add(window)
+            else:
+                seen.add(window)    
+        return list(result)
